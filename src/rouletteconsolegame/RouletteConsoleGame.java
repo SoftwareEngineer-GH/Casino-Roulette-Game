2ebac2150f74bd989db230c0ed9ef4ead305c48e
Software Engineer <chikonyem@gmail.com>
2020-11-26 00:58:50 +0200

    Displaying bet and winnings

--- src/rouletteconsolegame/RouletteConsoleGame.java
+++ src/rouletteconsolegame/RouletteConsoleGame.java
@@ -1,13 +1,28 @@
 package rouletteconsolegame;
 import java.util.Random;
+import java.util.Scanner;
 
 public class RouletteConsoleGame {
 	private static int rouletteNumber;
+	private static String betValue;
+	private static String outcome;
+	private static String player;
+	private static double winAmount;
+	private static double betAmount;
+	
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 		Random randomNum = new Random();
 		rouletteNumber = randomNum.nextInt(37);
 		
-		System.out.println(rouletteNumber);
+		System.out.print("Enter your bet number: ");
+		betValue=in.nextLine();
+		System.out.print("Enter your bet amount: ");
+		betAmount=in.nextDouble();
+		
+		System.out.printf("\nNumber: %d\n", rouletteNumber);		
+		System.out.println("Bet\t Outcome\t Winnings");		
+		System.out.println(betValue +"\t"+ outcome +"\t\t"+ winAmount);
 	}
 
 }
